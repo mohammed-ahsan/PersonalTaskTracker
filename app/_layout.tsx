e50d49bfd82a17ee3e762e5b483dc5e8c6171ba3
Mohammed Ahsan <earth.ahsan@gmail.com>
2025-06-18 08:35:59 +0600

    cc

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,7 +2,6 @@ import { TaskProvider } from '@/src/context/TaskContext';
 import AppNavigator from '@/src/navigation/AppNavigator';
 import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function RootLayout() {
